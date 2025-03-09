1efa17ec5537bffea5e5fdbc249c6f3b21ea1180
WillBahov <WillBahov@users.noreply.github.com>
2025-03-09 12:13:22 -0400

    test

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+      version = "1.69.0"
+    }
+  }
+}
+
+provider "databricks" {
+  # Configuration options 
+  
+}
